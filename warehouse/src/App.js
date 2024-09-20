7f4a712963746d12a236335517bb89632345275d
Kenny Ha <kjha9901@gmail.com>
2024-09-20 16:30:56 -0400

    InvLogStart

--- warehouse/src/App.js
+++ warehouse/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Login from './Login';
 import Welcome from './Welcome';
+import InventoryLog from './InventoryLog';
 
 function App() {
 
   return (
     <div>
-      <Login></Login>
-      <Welcome></Welcome>
+      <InventoryLog></InventoryLog>
     </div>
   );
 }
---
+++ warehouse/src/InventoryLog.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './css/InvLog.css';
+
+function App() {
+    
+    return (
+        <div className = "invlog">
+            <div className = "invlog-box">
+                <h2>Inventory History</h2>
+            </div>
+        </div>
+    );
+}
+
+export default App;
